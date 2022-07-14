bec58d4096b4435b455fd142d5cd3014fd4d6fc2
akquigley <98438324+akquigley@users.noreply.github.com>
2022-07-13 21:51:44 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+// for (i = 1; i < 11; i++) {
+//     console.log(i);
+// }
+
+// for (i = 10; i > 0; i--) {
+//     console.log(i);
+// }
+
+// let i = 1;
+// while (i < 11) {
+//     console.log(i);
+//     i++;
+// }
+
+// let i = 10;
+// while (i > 0) {
+//     console.log(i);
+//     i--;
+// }
+
+// let i = 1;
+// do {
+//     console.log(i);
+//     i++;
+// }
+// while (i < 11);
+
+// let i = 10;
+// do {
+//     console.log(i);
+//     i--;
+// }
+// while (i > 0);
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// for (let x of numbers) {
+//     console.log(x);
+// }
